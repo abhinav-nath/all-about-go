dbe14e3d46f8ac55ab99fda62c92fd8462869c56
Abhinav Nath <abhinavnath@ymail.com>
2022-05-05 23:59:47 +0530

    Add another example for Interface

---
+++ interfaces/area_calculator.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Shape interface {
+	Area() float64
+}
+
+type Rectangle struct {
+	Width, Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+func getArea(shape Shape) {
+
+	fmt.Println(shape.Area())
+}
+
+func main() {
+
+	r := Rectangle{Width: 7, Height: 8}
+	c := Circle{Radius: 5}
+
+	getArea(r)
+	getArea(c)
+}
